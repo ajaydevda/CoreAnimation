cf07e240524ab45af376d562a4096bf613071651
C711091 <ajaydevda8@gmail.com>
2019-12-20 19:07:46 +0530

    first commit

---
+++ coreGraphicsDemo/customButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  customButtonView.swift
+//  coreGraphicsDemo
+//
+//  Created by C711091 on 20/12/19.
+//  Copyright © 2019 icici. All rights reserved.
+//
+
+import UIKit
+
+class customButtonView: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ coreGraphicsDemo/meterView.swift
@@ -0,0 +1,21 @@
+//
+//  meterView.swift
+//  coreGraphicsDemo
+//
+//  Created by C711091 on 20/12/19.
+//  Copyright © 2019 icici. All rights reserved.
+//
+
+import UIKit
+
+class meterView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
