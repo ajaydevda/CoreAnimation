5d0e4d39e5b1b4d8fead541a01f17c4e512fa1fd
C711091 <ajaydevda8@gmail.com>
2019-12-27 18:16:31 +0530

    updated UI

--- coreGraphicsDemo/ViewController.swift
+++ coreGraphicsDemo/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var circularView: meterView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func subtractButton(_ sender: Any) {
+        
+        print("SUB")
+        circularView.counter -= 1
+    }
+    @IBAction func addButton(_ sender: Any) {
+    
+        print("ADD")
+         circularView.counter += 1
+    }
+    
+    @IBAction func webControllerBtn(_ sender: Any) {
+        
+        let storyBoard : UIStoryboard = UIStoryboard(name: "Main", bundle:nil)
+        let nextViewController = storyBoard.instantiateViewController(withIdentifier: "webcontroller") as! WebViewController
+        self.present(nextViewController, animated:true, completion:nil)
+        
+    }
 }
 
--- coreGraphicsDemo/meterView.swift
+++ coreGraphicsDemo/meterView.swift
@@ -8,14 +8,76 @@
 
 import UIKit
 
-class meterView: UIView {
+@ IBDesignable class meterView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private struct Constants {
+        static let numberOfGlasses = 15
+        static let lineWidth: CGFloat = 5.0
+        static let arcWidth: CGFloat = 100
+        
+        static var halfOfLineWidth: CGFloat {
+            return lineWidth / 2
+        }
     }
-    */
+    
+    @IBInspectable var counter: Int = 5 { didSet {
+    if counter <=  Constants.numberOfGlasses {
+    //the view needs to be refreshed
+    setNeedsDisplay()
+    }
+    }
+}
+    
+    @IBInspectable var outlineColor: UIColor = UIColor.blue
+    @IBInspectable var counterColor: UIColor = UIColor.orange
+    
+    override func draw(_ rect: CGRect) {
+        
+        let center = CGPoint(x: bounds.width / 2, y: bounds.height / 2)
+        // 2
+        let radius: CGFloat = max(bounds.width, bounds.height)
+        // 3
+        
+        let startAngle: CGFloat = 3 * .pi / 4
+        let endAngle: CGFloat = .pi / 4
+        print("center\(center)  \n radius\(radius) \n startAngle\(startAngle) \n endAngle\(endAngle)")
+        // 4
+        let path = UIBezierPath(arcCenter: center,
+                                radius: radius/2 - Constants.arcWidth/2,
+                                startAngle: startAngle,
+                                endAngle: endAngle,
+                                clockwise: true)
+        // 5
+        path.lineWidth = Constants.arcWidth
+        counterColor.setStroke()
+        path.stroke()
+        
+        let angleDifference: CGFloat = 2 * .pi - startAngle + endAngle
+        //then calculate the arc for each single glass
+        let arcLengthPerGlass = angleDifference / CGFloat(Constants.numberOfGlasses)
+        //then multiply out by the actual glasses drunk
+        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        //2 - draw the outer arc
+        let outlinePath = UIBezierPath(arcCenter: center,
+                                       radius: bounds.width/2 - Constants.halfOfLineWidth,
+                                       startAngle: startAngle,
+                                       endAngle: outlineEndAngle,
+                                       clockwise: true)
+        
+        //3 - draw the inner arc
+        outlinePath.addArc(withCenter: center,
+                           radius: radius/2 - Constants.arcWidth,
+                           startAngle: outlineEndAngle ,
+                           endAngle: startAngle,
+                           clockwise: false)
+        
+        print("bounds.width\(bounds.width/2) \n  Constants.arcWidth\(Constants.arcWidth) +Constants.halfOfLineWidth \(Constants.halfOfLineWidth)")
+        //4 - close the path
+        outlinePath.close()
+        outlineColor.setStroke()
+        outlinePath.lineWidth = Constants.lineWidth
+        outlinePath.stroke()
+    }
 
+    
 }
